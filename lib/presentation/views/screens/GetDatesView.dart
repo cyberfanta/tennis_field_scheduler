8ef57038d0f305bdaae96fd3f55e4f7e576c186b
Julio César León <julioleon2004@gmail.com>
2023-10-27 16:40:15 -0400

    Feat (Clean Architecture): Creating the initial structure of folder to make the clean architecture happen.

---
+++ lib/presentation/views/screens/GetDatesView.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class GetDatesView extends StatefulWidget {
+  const GetDatesView({super.key});
+
+  static const routeName = '/GetDatesView';
+
+  @override
+  State<GetDatesView> createState() => _GetDatesViewState();
+}
+
+class _GetDatesViewState extends State<GetDatesView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
