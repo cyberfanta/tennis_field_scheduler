24f2401ec3ee2cbeff96ef0692737f7947c8424e
Julio César León <julioleon2004@gmail.com>
2023-10-27 01:23:00 -0400

    Feat (Initial Commit): Base Project

---
+++ android/app/src/main/kotlin/com/example/tennis_field_scheduler/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tennis_field_scheduler
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    
+}
+rootProject.name = "tennis_field_scheduler"
+
