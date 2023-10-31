d645c37a7c46e8e68872a2fe53de4d277166289e
Julio César León <julioleon2004@gmail.com>
2023-10-31 02:06:12 -0400

    Feat (Shared Preferences): Disabling the backup in ios.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,18 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+    let documentsDirectory = paths[0]
+    let url = URL(fileURLWithPath: documentsDirectory)
+    do {
+        var resourceValues = URLResourceValues()
+        resourceValues.isExcludedFromBackup = true
+        try url.setResourceValues(resourceValues)
+    } catch {
+        print("Failed to set resource value: \(error)")
+    }
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
